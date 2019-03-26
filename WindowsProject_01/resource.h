86930bbf505c55e33d593226afc8091d78769da1
Artyom Hakobyan <aghakobyan@gmail.com>
2019-03-26 10:49:34 +0400

    rand Fixes

--- WindowsProject_01/resource.h
+++ WindowsProject_01/resource.h
@@ -8,6 +8,9 @@
 #define BWr                             1004
 #define IDC_SLIDER1                     1005
 #define Radiuss                         1005
+#define ImageWindow                     1006
+#define IDC_BUTTON1                     1007
+#define ImageUpload_b                   1007
 
 // Next default values for new objects
 // 
@@ -15,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
